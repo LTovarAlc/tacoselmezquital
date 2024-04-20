0267dc5edd4e1ebff20130f093e75726103c459c
Luis Tovar <ltovaralc@gmail.com>
2024-04-19 21:25:02 -0600

    fixed footer & icon

--- src/GlobalComponents/Footer/footer.jsx
+++ src/GlobalComponents/Footer/footer.jsx
@@ -3,9 +3,9 @@ import "./footer.css"
 const Footer = () => {
     return <footer className="footer">
         <img
-        src="../../../img/tacoselmezquital.png"
+        src="../../../img/tacosLogo.png"
         alt="Tacos El Mezquital"
-        className="logo__headerMobile"
+        className="logo__footer"
       />
       <span>Todos los derechos reservados &copy;</span>
     </footer>
