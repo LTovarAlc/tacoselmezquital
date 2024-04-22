6f69e962016fe1ee2e37d847e9f699df6eb422cf
Luis Tovar <ltovaralc@gmail.com>
2024-04-21 21:32:02 -0600

    fixed error in home

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -2,11 +2,7 @@ import AboutUs from "./About/about"
 import Banner from "./Banner/banner"
 import Location from "./location/location"
 import "./home.css"
-<<<<<<< HEAD
 import Menu from "./menu/menu"
-=======
-import Menu from "./menu/menu"
->>>>>>> dev
 
 const Home = () => {
     return(
