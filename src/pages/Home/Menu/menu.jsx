c6bbc0a770cac452cac06750ecdd281ae2a3c69f
Luis Tovar <ltovaralc@gmail.com>
2024-04-21 21:52:14 -0600

    error in home.jsx

--- src/pages/Home/Menu/menu.jsx
+++ src/pages/Home/Menu/menu.jsx
@@ -12,7 +12,7 @@ const Menu = () => {
           />
           <div className="menu-header__text">
             <h2 className="menu-title__name">
-              Tacos <br /> El Mezquital 2
+              Tacos <br /> El Mezquital
             </h2>
             <h1 className="menu-title">Menú</h1>
           </div>
