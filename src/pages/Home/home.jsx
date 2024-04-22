e3547fdac81508c12034c0a74376b221632b7158
Luis Tovar <ltovaralc@gmail.com>
2024-04-21 21:24:35 -0600

    fixed error

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -2,6 +2,11 @@ import AboutUs from "./About/about"
 import Banner from "./Banner/banner"
 import Location from "./location/location"
 import "./home.css"
+<<<<<<< HEAD
+import Menu from "./menu/menu"
+=======
+import Menu from "./menu/menu"
+>>>>>>> dev
 
 const Home = () => {
     return(
