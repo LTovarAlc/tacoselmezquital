83ff6942d8cda9c4bdf5fb0558a3bba22d68ee76
Luis Tovar <ltovaralc@gmail.com>
2024-04-21 22:05:09 -0600

    deleted menu

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -2,14 +2,12 @@ import AboutUs from "./About/about"
 import Banner from "./Banner/banner"
 import Location from "./location/location"
 import "./home.css"
-import Menu from "./menu/menu"
 
 const Home = () => {
     return(
         <section className="home">
             <Banner/>
             <AboutUs/>
-            <Menu/>
             <Location/>
         </section>
     )
