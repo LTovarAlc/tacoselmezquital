59e27b230ae1cd3eed4f969f7ae7af32ced22e58
Luis Tovar <ltovaralc@gmail.com>
2024-04-29 10:31:42 -0600

    add socials links

--- src/GlobalComponents/Footer/footer.jsx
+++ src/GlobalComponents/Footer/footer.jsx
@@ -1,14 +1,45 @@
-import "./footer.css"
+import "./footer.css";
+import facebook from "./footerimg/facebook.png";
+import instagram from "./footerimg/instagram.png";
+import whatsapp from "./footerimg/whatsapp.png";
 
 const Footer = () => {
-    return <footer className="footer">
+  return (
+    <footer className="footer">
+      <div className="footer-logo__container">
         <img
-        src="../../../img/tacosLogo.png"
-        alt="Tacos El Mezquital"
-        className="logo__footer"
-      />
-      <span>Todos los derechos reservados &copy;</span>
+          src="../../../img/tacosLogo.png"
+          alt="Tacos El Mezquital"
+          className="logo__footer"
+        />
+      </div>
+      <div className="socials-container">
+        <h3>¡Siguenos!</h3>
+        <ul className="social-list">
+          <a
+            target="_blank"
+            className="socials-link"
+            href="https://www.facebook.com/profile.php?id=61558697188866&viewas=&show_switched_toast=false&show_switched_tooltip=false&is_tour_dismissed=false&is_tour_completed=false&show_podcast_settings=false&show_community_review_changes=false&should_open_composer=false&badge_type=NEW_MEMBER&show_community_rollback_toast=false&show_community_rollback=false&show_follower_visibility_disclosure=false&bypass_exit_warning=true"
+          >
+            <img src={facebook} className="socialmedia-icon" />
+            <span>Tacos El Mezquital</span>
+          </a>
+          <a
+            target="_blank"
+            className="socials-link"
+            href="https://www.instagram.com/tacoselmezquital_/"
+          >
+            <img src={instagram} className="socialmedia-icon" />
+            <span>@tacoselmezquital_</span>
+          </a>
+          <a  target="_blank" className="socials-link" href="https://api.whatsapp.com/send?phone=8180892256">
+            <img src={whatsapp} className="socialmedia-icon" />
+            <span>+52 81 8089 2256</span>
+          </a>
+        </ul>
+      </div>
     </footer>
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/GlobalComponents/LocalCard/localCard.jsx
+++ src/GlobalComponents/LocalCard/localCard.jsx
@@ -5,13 +5,11 @@ const LocalCard = () => {
     <div className="localCard">
       <h3>Antiguo Camino al Mezquital</h3>
       <iframe
-        className="map"
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d898.3574809476444!2d-100.2486025997081!3d25.75635942579072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2smx!4v1712775515706!5m2!1ses!2smx"
-        width="600"
-        height="450"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3593.431633978469!2d-100.25106282549103!3d25.75630290882982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8662ebddffc88b03%3A0x98d378c890cb7e6f!2sTacos%20%22El%20Mezquital%22!5e0!3m2!1ses!2smx!4v1714406498610!5m2!1ses!2smx"
         allowFullScreen=""
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
+        className="map"
       ></iframe>
       <div className="local__info-container">
         <div className="local__info">
